a2eb60940f2cbc22e7b37b5beea6379d850a8709
hakangl-software <hglbilisim@gmail.com>
2025-11-02 22:28:30 +0300

    02112025-2238

---
+++ 38.String Metotları/app.js
@@ -0,0 +1,32 @@
+
+
+//? STRING METOTLARI
+
+/*
+    charAt()        : Verdiğin index numarasına göre sana karşılığındaki karakteri verir.
+    concat()        : İki string ifadeyi birleştirir. 
+    indexof()       : Vermiş olduğun değerin index numarasını verir.
+    lastindexof()   : 
+
+
+
+
+
+*/
+
+    let kurs = "Modern Web Geliştirme Kursu";
+    let tarih = "2025";
+
+
+    // CHARAT METOT KULLANIMI
+    // let karakter = kurs.charAt(5);
+    // console.log(karakter);
+
+    // CONCAT METOT KULLANIMI
+    //let sonuc = kurs.concat(tarih);
+    //console.log(sonuc);
+    //Burada stringleri birleştirir. Kopyasını return ederç tanımladığım sonuca aktarır.
+
+    // INDEXOF METOT
+    // let index = kurs.indexOf("o");
+    // console.log(index);
