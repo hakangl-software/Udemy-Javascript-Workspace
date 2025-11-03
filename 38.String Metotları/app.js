bbe14e56da042b90040f0e09b6fc580ea83b196b
hakangl-software <hglbilisim@gmail.com>
2025-11-03 22:53:51 +0300

    03112025-2253

--- 38.String Metotları/app.js
+++ 38.String Metotları/app.js
@@ -6,10 +6,17 @@
     charAt()        : Verdiğin index numarasına göre sana karşılığındaki karakteri verir.
     concat()        : İki string ifadeyi birleştirir. 
     indexof()       : Vermiş olduğun değerin index numarasını verir.
-    lastindexof()   : 
-
-
-
+    lastindexof()   : Son 
+    toUpperCase()   : Dizideki bütün harfleri büyük harfe çevirir.
+    toLowerCase()   : Dizideki bütün harfleri küçük harfe çevirir.
+    trim()          : Dizideki boşlukları kırpar.
+    slice()         : Dilimleme işleminde kullanılır.
+    sunstring()     : Slice metodu ile aynı şey sadece index farkı vardır.
+    replace()       : Yer değiştirmek anlamı var. String değerlerini değiştirip return eder.
+    split()         : Dizideki belirlemiş olduğunuz ayraça göre ayırıp dilim dilim dizi haline getirir.
+    valueOf()       :
+    startSwith()    : Dizinin verdiğiniz değerle başlayıp başlamadığını gösterir. Boolean döner.
+    endSwith()      : Dizinin verdiğiniz değerle bitip bitediğini gösterir. Boolean döner.
 
 
 */
@@ -30,3 +37,49 @@
     // INDEXOF METOT
     // let index = kurs.indexOf("o");
     // console.log(index);
+
+    // LASTINDEXOF METOT
+    // let sonIndex = kurs.lastIndexOf("s");
+    // console.log(sonIndex);
+
+    // TOUPPERCASE METOT
+    // let buyukHarf = kurs.toUpperCase();
+    // console.log(kurs);
+    // console.log(buyukHarf);
+
+    // TOLOWERCASE METOT
+    // let kucukHarf = kurs.toLowerCase();
+    // console.log(kurs);
+    // console.log(kucukHarf);
+
+    // TRIM METOT
+    //console.log(kurs.trim());
+
+    // SLICE METOT
+    // console.log(kurs);
+    // console.log(kurs.slice(7,10)); // Başlangıç ve bitiş index numarasını (dahil) yazılır.
+
+    // SUBSTRING METOT
+    // console.log(kurs);
+    // console.log(kurs.substring(0,5)); // Başlangıç ve bitiş index numarasını (hariç) yazılır.
+
+    // REPLACE METOT
+    // console.log(kurs);
+    // console.log(kurs.replace("Modern","Güncel"));
+
+    // SPLIT METOT
+    // console.log(kurs);
+    // let dizi = console.log(kurs.split(" "));
+    // console.log(dizi);
+
+    // STARTSWITH METOT
+    // console.log(kurs);
+    // console.log(kurs.startsWith("M"));
+
+    // ENDSWITH METOT
+    // console.log(kurs);
+    // console.log(kurs.endsWith("u"));
+
+
+
+
---
+++ 39. Math Metotları/app.js
@@ -0,0 +1,41 @@
+
+
+//? Math Metotları
+
+/*
+    1 - Floor   : Küsüratı atar.
+    2 - Ceil    : Küsürata bakmadan bir üste yuvarlar. 
+    3 - Round   : Küsüratlı sayılarda en yakın tam sayıya yuvarlar.
+
+    4 - Max     : Number dizideki sayıarın en büyüğünü verir.
+    5 - Min     : Number dizisindeki sayıların en küçüğünü verir.
+
+    6 - Random  : Rastgele değerler için kullanılan bir metottur.
+    7 - Abs     : Mutlak değer almak için kullanılır.
+    8 - Sqrt    : Karekökünü almak için kullanılır.
+    9 - Pow     : Sayının üssünü almak için kullanılır.
+    PI Sayısı   : PI sayısını alır.
+
+
+
+
+
+*/
+
+
+let a = 3.15;
+let b = -12;
+// console.log(Math.floor(a));
+// console.log(Math.ceil(a)) ;
+// console.log(Math.round(a));
+
+// console.log(Math.max(1,2,3,4,5,6));
+// console.log(Math.min(1,2,3,4,5,6));
+
+// console.log(Math.abs(b));
+// console.log(Math.sqrt(9));
+// console.log(Math.pow(3,4));
+// console.log(Math.PI);
+
+
+// console.log(Math.floor(Math.random()*100));  RANDOMM ÖRNEK
